c8381121ec8cc9746f7186805e724f78c7f7d552
wilsonkl4 <wilsonkl4@etsu.edu>
2023-02-21 20:40:55 -0500

    -Added in functionality to eventually pull all sentiment entries from DB -Added in mock sample sentiments so page will load for now -Main page shows list of sentiments (will be pulled from DB) with option to delete (not yet implemented) and option to classify text (not yet implemented)

--- client_gui/ClientGUI/ClientGUI/Controllers/HomeController.cs
+++ client_gui/ClientGUI/ClientGUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using ClientGUI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace ClientGUI.Controllers
@@ -8,14 +9,37 @@ namespace ClientGUI.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private string SENTIMENT_SOURCE = @"https://localhost:1234/analyze";
+        private string DATABASE_SOURCE = @"https://localhost:1234/";
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<SentimentModel> sentiments = new List<SentimentModel>();
+
+            //Need to have database set up - for now, I'll just hardcode some in
+            /* 
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(DATABASE_SOURCE))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+
+                    sentiments = JsonConvert.DeserializeObject<List<SentimentModel>>(apiResponse);
+                }
+            }
+            */
+
+            //Dummy sentiment data
+            sentiments.Add(new SentimentModel { Id = 1, Timestamp = new DateTime(2023, 2, 21, 20, 28, 0), TextSearched = "example", SentimentResult = "postive", PercentageScore = 0.23});
+            sentiments.Add(new SentimentModel { Id = 2, Timestamp = new DateTime(2023, 2, 21, 20, 29, 0), TextSearched = "test", SentimentResult = "negative", PercentageScore = 0.57});
+            sentiments.Add(new SentimentModel { Id = 3, Timestamp = new DateTime(2023, 2, 21, 20, 30, 0), TextSearched = "another", SentimentResult = "neutral", PercentageScore = 0.98});
+
+            return View(sentiments);
         }
     }
 }
