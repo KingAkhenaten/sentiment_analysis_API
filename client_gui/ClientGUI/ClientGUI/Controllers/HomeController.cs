c223b4d81217b01b1d6c70808efb9ceb93b972c6
wilsonkl4 <wilsonkl4@etsu.edu>
2023-02-21 20:15:04 -0500

    -Create basic MVC web app structure -Add Model for sentiment analysis object

---
+++ client_gui/ClientGUI/ClientGUI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using ClientGUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ClientGUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ client_gui/ClientGUI/ClientGUI/Models/SentimentModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ClientGUI.Models
+{
+    public class SentimentModel
+    {
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string? TextSearched { get; set; }
+
+        public string? SentimentResult { get; set; }
+
+        public double PercentageScore { get; set; }
+    }
+}
