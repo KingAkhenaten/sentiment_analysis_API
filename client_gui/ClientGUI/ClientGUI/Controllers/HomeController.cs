32cdbfec13102f489f3ca82781691f54e965fdca
wilsonkl4 <wilsonkl4@etsu.edu>
2023-02-26 23:28:02 -0500

    -Fixed connection with API - now successful

--- client_gui/ClientGUI/ClientGUI/Controllers/HomeController.cs
+++ client_gui/ClientGUI/ClientGUI/Controllers/HomeController.cs
@@ -10,7 +10,8 @@ namespace ClientGUI.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        private string SENTIMENT_SOURCE = @"http://host.docker.internal:8000/analyze"; //@"https://localhost:8000/analyze";
+        private string SENTIMENT_SOURCE = @"http://host.docker.internal:8000/analyze";
+        //private string SENTIMENT_SOURCE = @"https://localhost:8000/analyze";
         private string DATABASE_SOURCE = @"https://localhost:1234/";
 
         public HomeController(ILogger<HomeController> logger)
@@ -57,12 +58,16 @@ namespace ClientGUI.Controllers
             {
                 //Package up the sentence to send
                 //StringContent content = new StringContent(s.Sentence, Encoding.UTF8, "application/json");
-                StringContent content = new StringContent(JsonConvert.SerializeObject(s), Encoding.UTF8, "application/json");
+                string json = "{\"sentence\": \"" + s.Sentence + "\"}";
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                //StringContent content = new StringContent(JsonConvert.SerializeObject(s), Encoding.UTF8, "application/json");
 
                 //Send it to the API and ask to analyze
                 using (var response = await httpClient.PostAsync(SENTIMENT_SOURCE, content))
                 {
                     System.Diagnostics.Debug.WriteLine($"Response: {response.ToString()}");
+                    string res = await response.Content.ReadAsStringAsync();
+                    System.Diagnostics.Debug.WriteLine($"{res}");
 
                     /*
                     //Receive analysis back + package to send to DB
