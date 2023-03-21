70ac611e2d1b09344b96f5165a7e36f611692a4c
Christian Clay <clayck@etsu.edu>
2023-03-21 13:38:29 -0400

    comment

--- sentiment_analysis/server.py
+++ sentiment_analysis/server.py
@@ -3,7 +3,7 @@ from sentiment_analysis.src.nltk_utils import do_analysis, format_analysis
 
 app = Flask(__name__)
 
-
+# test comment to test github actions
 @app.post('/analyze')
 def analyze():
     data = request.json
