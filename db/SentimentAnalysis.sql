85e88f93841fc5dac4a9c4e064d218b723ee9816
Christian Clay <clayck@etsu.edu>
2023-03-21 13:32:05 -0400

    created integration branch for sentiment analysis

--- db/SentimentAnalysis.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE SentimentAnalysis (
-    ID SERIAL PRIMARY KEY,
-    TimeStamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    Text varchar(1000),
-    SentimentScore varchar(255),
-    SentimentPercentage float(4)
-);
