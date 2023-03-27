7f1079bc80fa5e22f3ae32c12d8e3b1c386df748
wilsonkl4 <wilsonkl4@etsu.edu>
2023-03-27 16:57:23 -0400

    -Some WIP on unit testing... trying to figure out how to get the data when a view is returned

--- client_gui/ClientGUI/ClientGUIUnitTesting/ClientGUIUnitTests.cs
+++ client_gui/ClientGUI/ClientGUIUnitTesting/ClientGUIUnitTests.cs
@@ -1,4 +1,8 @@
 using ClientGUI.Controllers;
+using ClientGUI.Models;
+using ClientGUI.Connectors;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ClientGUIUnitTesting
 {
@@ -9,13 +13,58 @@ namespace ClientGUIUnitTesting
         [SetUp]
         public void Setup()
         {
-            sut = new HomeController();
         }
 
         [Test]
-        public void Test1()
+        public void IndexShouldReturnListOfSentiments()
         {
-            Assert.Pass();
+            var mockDataSource = new Mock<IDataSource>();
+            mockDataSource.Setup(x => x.GetSentiments()).Returns(GetTestListOfSentiments());
+
+            var mockSentimentAnalyzer = new Mock<ISentiment>();
+
+            var sut = new HomeController(mockDataSource.Object, mockSentimentAnalyzer.Object);
+
+            Task<IActionResult> result = sut.Index();
+            IActionResult actionResult = result.Result;
+
+            //Assert that the page loaded correctly
+            Assert.IsInstanceOf(typeof(OkResult), actionResult);
+        }
+
+        private List<SentimentModel> GetTestListOfSentiments()
+        {
+            List<SentimentModel> sentiments = new List<SentimentModel>();
+
+            sentiments.Add(
+                new SentimentModel
+                {
+                    Id = 0,
+                    Timestamp = new DateTime(2023, 03, 27),
+                    TextSearched = "test sentence",
+                    SentimentResult = "neutral",
+                    PercentageScore = 0.5
+                });
+            sentiments.Add(
+                new SentimentModel
+                {
+                    Id = 1,
+                    Timestamp = new DateTime(2023, 03, 27),
+                    TextSearched = "i like sentences",
+                    SentimentResult = "positive",
+                    PercentageScore = 0.7
+                });
+            sentiments.Add(
+                new SentimentModel
+                {
+                    Id = 2,
+                    Timestamp = new DateTime(2023, 03, 27),
+                    TextSearched = "i hate sentences",
+                    SentimentResult = "negative",
+                    PercentageScore = 0.2
+                });
+
+            return sentiments;
         }
     }
 }
