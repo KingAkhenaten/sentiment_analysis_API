afd6160a4e5a8721ab88ab39582295ce728edb27
wilsonkl4 <wilsonkl4@etsu.edu>
2023-03-27 18:26:48 -0400

    -First Unit test for Index completed

--- client_gui/ClientGUI/ClientGUIUnitTesting/ClientGUIUnitTests.cs
+++ client_gui/ClientGUI/ClientGUIUnitTesting/ClientGUIUnitTests.cs
@@ -18,18 +18,40 @@ namespace ClientGUIUnitTesting
         [Test]
         public void IndexShouldReturnListOfSentiments()
         {
+            //Mock the database and sentiment services using Moq
             var mockDataSource = new Mock<IDataSource>();
             mockDataSource.Setup(x => x.GetSentiments()).Returns(GetTestListOfSentiments());
 
             var mockSentimentAnalyzer = new Mock<ISentiment>();
 
+            //Create the system under test (sut) - the HomeController, using
+            //the mocked services
             var sut = new HomeController(mockDataSource.Object, mockSentimentAnalyzer.Object);
 
-            Task<IActionResult> result = sut.Index();
-            IActionResult actionResult = result.Result;
+            //Call the Index method
+            var actionResult = sut.Index().Result;
 
-            //Assert that the page loaded correctly
-            Assert.IsInstanceOf(typeof(OkResult), actionResult);
+            //Ensure the view is returned
+            var view = actionResult as ViewResult;
+            Assert.NotNull(view);
+
+            //Ensure the model is of the correct data type
+            var model = view.Model as List<SentimentModel>;
+            Assert.NotNull(model);
+
+            //Assert that there are the correct amount of sentiments in the model
+            Assert.That(model.Count, Is.EqualTo(3));
+
+            //Assert that the model contains the correct list of sentiments
+            List<SentimentModel> testSentiments = GetTestListOfSentiments();
+            for (int i = 0; i < model.Count; i++)
+            {
+                Assert.That(model[i].Id, Is.EqualTo(testSentiments[i].Id));
+                Assert.That(model[i].TextSearched, Is.EqualTo(testSentiments[i].TextSearched));
+                Assert.That(model[i].SentimentResult, Is.EqualTo(testSentiments[i].SentimentResult));
+                Assert.That(model[i].Timestamp, Is.EqualTo(testSentiments[i].Timestamp));
+                Assert.That(model[i].PercentageScore, Is.EqualTo(testSentiments[i].PercentageScore));
+            }
         }
 
         private List<SentimentModel> GetTestListOfSentiments()
