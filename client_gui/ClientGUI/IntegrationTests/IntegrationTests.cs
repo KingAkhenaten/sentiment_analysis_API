b57bc5a7fdc2343c3c6cd8fc43625008b5717438
Caleb Ishola <ISHOLAA@ETSU.EDU>
2023-04-02 12:43:54 -0400

    Merged into same project and added a projec reference

--- client_gui/ClientGUI/IntegrationTests/IntegrationTests.cs
+++ client_gui/ClientGUI/IntegrationTests/IntegrationTests.cs
@@ -1,3 +1,7 @@
+using ClientGUI.Connectors;
+using ClientGUI.Controllers;
+using ClientGUI.Models;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using Npgsql;
 using System.Net;
@@ -9,12 +13,12 @@ using System.Text;
 
 namespace IntegrationTest
 {
-    public class Tests
+    public class IntegrationTests
     {
-        private const string PythonApiUrl = @"http://5400-project-sentiment_analysis-1:8000/analyze";
-        //private const string PythonApiUrl = @"http://localhost:8000/analyze";
-        private const string DbConnectionString = "Server=5400-project-db-1;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;";
-        //private const string DbConnectionString = "Server=localhost:5432;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;";
+        //private const string PythonApiUrl = @"http://5400-project-sentiment_analysis-1:8000/analyze";
+        private const string PythonApiUrl = @"http://localhost:8000/analyze";
+        //private const string DbConnectionString = "Server=5400-project-db-1;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;";
+        private const string DbConnectionString = "Server=localhost:5432;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;";
 
         private HttpClient _httpClient;
         private NpgsqlConnection conn;
@@ -410,27 +414,189 @@ namespace IntegrationTest
 
 
         }
+        [TearDown]
+        public void RemoveAllItems()
+        {
+            //Create the system under test (sut) - the HomeController, using the real services
+            //var sut = new HomeController(new PostgresDataSource(), new SentimentAnalyzer());
+            var sut = new HomeController(new PostgresDataSource("Server=localhost;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;"), new SentimentAnalyzer(@"http://localhost:8000/analyze"));
+
+            //Call the Index method to get all the sentiments
+            var actionResult = sut.Index().Result;
+            var view = actionResult as ViewResult;
+            var model = view.Model as List<SentimentModel>;
+
+            //Delete them all
+            if (model == null)
+                return;
+
+            for (int i = 0; i < model.Count; i++)
+            {
+                var r = sut.Delete(model[i].Id).Result;
+            }
+        }
+
+        [Test]
+        public void IndexShouldReturnEmptyListOfSentimentsWhenNew()
+        {
+            //Create the system under test (sut) - the HomeController, using the real services
+            //var sut = new HomeController(new PostgresDataSource(), new SentimentAnalyzer());
+            var sut = new HomeController(new PostgresDataSource("Server=localhost;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;"), new SentimentAnalyzer(@"http://localhost:8000/analyze"));
+
+            //Call the Index method
+            var actionResult = sut.Index().Result;
+
+            //Ensure the view is returned
+            var view = actionResult as ViewResult;
+            Assert.NotNull(view);
+
+            //Ensure that the view is for the Index page
+            Assert.That(view.ViewName, Is.EqualTo("Index"));
+
+            //Since there are no sentiments, it should be null
+            var model = view.Model as List<SentimentModel>;
+            Assert.Null(model);
+        }
+
+        [Test]
+        public void IndexShouldReturnListOfSentimentsWhenSomeAdded()
+        {
+            //Create the system under test (sut) - the HomeController, using the real services
+            //var sut = new HomeController(new PostgresDataSource(), new SentimentAnalyzer());
+            var sut = new HomeController(new PostgresDataSource("Server=localhost;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;"), new SentimentAnalyzer(@"http://localhost:8000/analyze"));
+
+
+            //Create and add 2 sentiments
+            List<SentenceModel> sentences = new List<SentenceModel>();
+            sentences.Add(new SentenceModel { Sentence = "i love sentences" });
+            sentences.Add(new SentenceModel { Sentence = "i hate sentences" });
+            var r1 = sut.Create(sentences[0]).Result;
+            var r2 = sut.Create(sentences[1]).Result;
+
+            //Call the Index method
+            var actionResult = sut.Index().Result;
+
+            //Ensure the view is returned
+            var view = actionResult as ViewResult;
+            Assert.NotNull(view);
+
+            //Ensure that the view is for the Index page
+            Assert.That(view.ViewName, Is.EqualTo("Index"));
+
+            //Ensure the model is of the correct data type
+            var model = view.Model as List<SentimentModel>;
+            Assert.NotNull(model);
+
+            //Assert that there are the correct amount of sentiments in the model - should be 2 since we added 2
+            Assert.That(model.Count, Is.EqualTo(2));
+
+            //Assert that the model contains the correct list of sentiments
+            for (int i = 0; i < model.Count; i++)
+            {
+                Assert.That(model[i].TextSearched, Is.EqualTo(sentences[i].Sentence));
+            }
+        }
+
+        [Test]
+        public void CreateNoArgsShouldReturnCreateView()
+        {
+            //Create the system under test (sut) - the HomeController, using the real services
+            //var sut = new HomeController(new PostgresDataSource(), new SentimentAnalyzer());
+            var sut = new HomeController(new PostgresDataSource("Server=localhost;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;"), new SentimentAnalyzer(@"http://localhost:8000/analyze"));
+
+            //Call the create method
+            var actionResult = sut.Create();
+
+            //Ensure the view is returned
+            var view = actionResult as ViewResult;
+            Assert.NotNull(view);
+
+            //Ensure that the view is for the Create page
+            Assert.That(view.ViewName, Is.EqualTo("Create"));
+        }
+
+        [Test]
+        public void CreateShouldCreateAndAddSentiment()
+        {
+            SentenceModel s = new SentenceModel { Sentence = "test sentence" };
+
+            //Create the system under test (sut) - the HomeController, using the real services
+            //var sut = new HomeController(new PostgresDataSource(), new SentimentAnalyzer());
+            var sut = new HomeController(new PostgresDataSource("Server=localhost;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;"), new SentimentAnalyzer(@"http://localhost:8000/analyze"));
+
+            //Call the Create method
+            var actionResult = sut.Create(s).Result;
+
+            //Ensure that we redirect
+            var redirect = actionResult as RedirectToActionResult;
+            Assert.NotNull(redirect);
+
+            //Ensure that the redirect is for the Index page
+            Assert.That(redirect.ActionName, Is.EqualTo("Index"));
+
+            //Call the Index method
+            actionResult = sut.Index().Result;
+
+            //Ensure the view is returned
+            var view = actionResult as ViewResult;
+            Assert.NotNull(view);
+
+            //Ensure that the view is for the Index page
+            Assert.That(view.ViewName, Is.EqualTo("Index"));
+
+            //Ensure the model is of the correct data type
+            var model = view.Model as List<SentimentModel>;
+            Assert.NotNull(model);
+
+            //Assert that there are the correct amount of sentiments in the model - should be 1, the one we added with create
+            Assert.That(model.Count, Is.EqualTo(1));
+
+            //Assert that the model contains the correct sentiment object
+            Assert.That(model[0].TextSearched, Is.EqualTo(s.Sentence));
+        }
+
+        [Test]
+        public void DeleteShouldRemoveSentiment()
+        {
+            //Create the system under test (sut) - the HomeController, using the real services
+            //var sut = new HomeController(new PostgresDataSource(), new SentimentAnalyzer());
+            var sut = new HomeController(new PostgresDataSource("Server=localhost;Port=5432;Database=DataAnalysis;User Id=root;Password=CSCI5400;"), new SentimentAnalyzer(@"http://localhost:8000/analyze"));
+
+            //Create a sentence, which we can create a sentiment out of to be able to delete it
+            var r1 = sut.Create(new SentenceModel { Sentence = "i love sentences" }).Result;
+
+            //Get the sentiments to figure out its Id
+            var result = sut.Index().Result;
+            var v = result as ViewResult;
+            var m = v.Model as List<SentimentModel>;
+
+            //Call the Delete method
+            var actionResult = sut.Delete(m[0].Id).Result;
+
+            //Ensure that we redirect
+            var redirect = actionResult as RedirectToActionResult;
+            Assert.NotNull(redirect);
+
+            //Ensure that the redirect is for the Index page
+            Assert.That(redirect.ActionName, Is.EqualTo("Index"));
+
+            //Call the Index method
+            actionResult = sut.Index().Result;
+
+            //Ensure the view is returned
+            var view = actionResult as ViewResult;
+            Assert.NotNull(view);
+
+            //Ensure that the view is for the Index page
+            Assert.That(view.ViewName, Is.EqualTo("Index"));
+
+            //Model should be null since there shouldn't be anything in there
+            var model = view.Model as List<SentimentModel>;
+            Assert.Null(model);
+        }
 
 
 
-
-        //[Test]
-        //public async Task SendRequesToPerformAnalysis()
-        //{
-
-        //    // Send a request from the C# GUI to the Python API to perform sentiment analysis on a sample text
-        //    var request = new HttpRequestMessage(HttpMethod.Post, PythonApiUrl);
-        //    request.Content = new StringContent($"{{\"sentence\":\"{"I hate apples"}\"}}", Encoding.UTF8, "application/json");
-        //    var response = await _httpClient.SendAsync(request);
-
-
-        //    // Verify that the Python API returns the correct sentiment analysis results
-        //    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-        //    var responseContent = await response.Content.ReadAsStringAsync();
-        //    Assert.That(responseContent, Is.EqualTo("{\"sentiment\":\"neg\"}"));
-
-
-        //}
 
 
     }
