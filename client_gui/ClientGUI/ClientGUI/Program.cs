55e348b3fd3073d42364b78e93b4a1e679827399
wilsonkl4 <wilsonkl4@etsu.edu>
2023-04-02 20:20:02 -0400

    -test commit to break code to test workflow

--- client_gui/ClientGUI/ClientGUI/Program.cs
+++ client_gui/ClientGUI/ClientGUI/Program.cs
@@ -6,7 +6,7 @@ namespace ClientGUI
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            //var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddControllersWithViews(); 
