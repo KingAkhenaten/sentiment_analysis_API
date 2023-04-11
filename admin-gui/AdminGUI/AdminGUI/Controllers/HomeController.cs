404f34a83ac9e8cc987f37dfe3d59ec766d84e7a
wilsonkl4 <wilsonkl4@etsu.edu>
2023-04-11 15:21:30 -0400

    -cleanup of extra components

--- admin-gui/AdminGUI/AdminGUI/Controllers/HomeController.cs
+++ admin-gui/AdminGUI/AdminGUI/Controllers/HomeController.cs
@@ -6,27 +6,13 @@ namespace AdminGUI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- admin-gui/AdminGUI/AdminGUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AdminGUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
