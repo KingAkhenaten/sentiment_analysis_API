9d7df606f6e63e2da8e854133f4e8ca891e399c1
wilsonkl4 <wilsonkl4@etsu.edu>
2023-04-11 15:37:56 -0400

    -added basic structure for maintenance page

--- admin-gui/AdminGUI/AdminGUI/Controllers/HomeController.cs
+++ admin-gui/AdminGUI/AdminGUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using AdminGUI.Models;
+﻿using AdminGUI.Connectors;
+using AdminGUI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,13 +7,29 @@ namespace AdminGUI.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
+        private IDataSource _dataSource;
+
+        public HomeController(IDataSource dataSource)
         {
+            _dataSource = dataSource;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Maintenance()
+        {
+            //Query the database for the sentiments
+            List<SentimentModel>? sentiments = _dataSource.GetSentiments();
+
+            //If there are no sentiments, set the List to null
+            if (sentiments.Count == 0)
+                sentiments = null;
+
+            //Return the maintenance page view, showing the queried sentiments in the list view
+            return View("Maintenance", sentiments);
         }
     }
 }
---
+++ admin-gui/AdminGUI/AdminGUI/Models/SentenceModel.cs
@@ -0,0 +1,19 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Solution/Project:  AdminGUI - Sentiment Analysis Project
+//	File Name:         SentenceModel.cs
+//	Description:       Defines the data model for a sentence, which is just the text of the sentence to send to the
+//	                   API.
+//	Course:            CSCI-5400 - Software Production
+//	Author:            Katie Wilson, wilsonkl4@etsu.edu, East Tennessee State University
+//	Last Modified:     02/28/23
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace AdminGUI.Models
+{
+    public class SentenceModel
+    {
+        public string? Sentence { get; set; }
+    }
+}
