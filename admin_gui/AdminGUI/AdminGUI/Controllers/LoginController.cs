be6dd855ccf292af3233cce6010075acc925d0c1
Christian Clay <55149123+KingAkhenaten@users.noreply.github.com>
2023-04-12 23:03:28 -0400

    added api for login

---
+++ admin_gui/AdminGUI/AdminGUI/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using AdminGUI.Services;
+
+namespace AdminGUI.Controllers;
+
+public class LoginController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login([FromServices] LoginCommand loginCommand, [FromBody] LoginRequest request )
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        var executeStatus = loginCommand.Execute(request.Username, request.Password);
+        if (!executeStatus)
+        {
+            return BadRequest(executeStatus);
+        }
+
+        return Ok();
+
+    }
+}
+
+public class LoginRequest
+{
+    public LoginRequest(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+
+    [Required] public string Username { get; set; }
+    [Required] public string Password { get; set; }
+}
